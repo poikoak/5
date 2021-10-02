d0126ce9bc7807a62bc90dba3803fd90bfd5a9ca
poikoak <87008326+poikoak@users.noreply.github.com>
2021-10-02 21:48:46 +0300

    Add files via upload

---
+++ 1/WpfKinopoiskLinkSQL/WpfKinopoiskLinkSQL/Window1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfKinopoiskLinkSQL
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public string yesno;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            yesno = "Yes";
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            yesno = "Yes";
+            this.Close();
+        }
+    }
+}
